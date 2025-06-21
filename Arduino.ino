3e39702acd6de7b69a62a6645ffa09fe83cd845b
Chandan Tiwary <115684022+Elric-xD@users.noreply.github.com>
2025-06-21 21:14:00 +0530

    Update Arduino.ino

--- Arduino.ino
+++ Arduino.ino
@@ -1,139 +1,98 @@
-int del=1000;
+// Pin assignments
+const int sensorPins[4] = {A0, A1, A2, A3};
+const int greenPins[4] = {3, 6, 9, 12};
+const int yellowPins[4] = {4, 7, 10, 13};
+const int redPins[4] = {5, 8, 11, A4}; // A4 used for red of lane 4
+const int emergencyPin = 2;
+
+// Traffic control timings (ms)
+const unsigned long baseGreen = 10000;
+const unsigned long maxGreen = 30000;
+const unsigned long yellowTime = 3000;
+
+// Sensor & traffic thresholds
+const int sensorThreshold = 300;
+const int heavyThreshold = 600;
+
+// --- Function Declarations ---
+void handleEmergency();
+void cycleSignal(int lane, unsigned long greenTime);
 
 void setup() {
-
-  // put your setup code here, to run once:
-
-  pinMode(7,OUTPUT);
-
-  pinMode(9,INPUT);
-
-pinMode(8,INPUT);
-
-pinMode(11,OUTPUT);
-
-pinMode(6,OUTPUT);
-
-pinMode(2, INPUT);
-
-pinMode(4,INPUT);
-
-pinMode(A3,INPUT);
-
-Serial.begin(9600);
-
+  Serial.begin(9600);
+  for (int i = 0; i < 4; i++) {
+    pinMode(sensorPins[i], INPUT);
+    pinMode(greenPins[i], OUTPUT);
+    pinMode(yellowPins[i], OUTPUT);
+    pinMode(redPins[i], OUTPUT);
+    digitalWrite(greenPins[i], LOW);
+    digitalWrite(yellowPins[i], LOW);
+    digitalWrite(redPins[i], HIGH); // Start all red
+  }
+  pinMode(emergencyPin, INPUT_PULLUP);
 }
 
-void re1(){ //noTone(11);
-
- // digitalWrite(13,HIGH);
-
-   delay(del);
-
-  digitalWrite(6,HIGH);
-
-  digitalWrite(7,LOW);
-
-  while(digitalRead(8)==HIGH && digitalRead(9)==LOW){
-
-    digitalWrite(6,HIGH);
-
-  digitalWrite(7,LOW);
-
+void loop() {
+  if (digitalRead(emergencyPin) == LOW) {
+    handleEmergency();
+    return;
   }
 
-
-
-  delay(del);
-
-
-
-}
-
-void re2(){//digitalWrite(13,HIGH);
-
-//noTone(11);
-
-  delay(del);
-
-  digitalWrite(6,LOW);
-
-  digitalWrite(7,HIGH);
-
-   while(digitalRead(8)==LOW && digitalRead(9)==HIGH){
-
-    digitalWrite(6,LOW);
-
-  digitalWrite(7,HIGH);
-
+  int densities[4];
+  for (int i = 0; i < 4; i++) {
+    densities[i] = analogRead(sensorPins[i]);
+    Serial.print("Lane "); Serial.print(i);
+    Serial.print(": "); Serial.println(densities[i]);
   }
 
- delay(del);
+  int maxLane = 0;
+  for (int i = 1; i < 4; i++) {
+    if (densities[i] > densities[maxLane]) maxLane = i;
+  }
 
+  unsigned long greenTime = baseGreen;
+  if (densities[maxLane] > heavyThreshold) {
+    greenTime = min(maxGreen, map(densities[maxLane], heavyThreshold, 1023, baseGreen, maxGreen));
+  }
+
+  cycleSignal(maxLane, greenTime);
 }
 
-bool ra=0;
+void cycleSignal(int lane, unsigned long greenTime) {
+  for (int i = 0; i < 4; i++) {
+    if (i == lane) {
+      digitalWrite(redPins[i], LOW);
+      digitalWrite(greenPins[i], HIGH);
+    } else {
+      digitalWrite(greenPins[i], LOW);
+      digitalWrite(yellowPins[i], LOW);
+      digitalWrite(redPins[i], HIGH);
+    }
+  }
 
-void loop() {//digitalWrite(13,LOW);
-
-int ra = analogRead(A3);
-
-if(ra>1000){
-
-  del=5000;
-
+  delay(greenTime);
+  digitalWrite(greenPins[lane], LOW);
+  digitalWrite(yellowPins[lane], HIGH);
+  delay(yellowTime);
+  digitalWrite(yellowPins[lane], LOW);
+  digitalWrite(redPins[lane], HIGH);
 }
 
-else
+void handleEmergency() {
+  Serial.println("Emergency detected! Clearing intersection...");
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(greenPins[i], LOW);
+    digitalWrite(yellowPins[i], LOW);
+    digitalWrite(redPins[i], LOW);
+  }
 
-del=1000;
+  unsigned long startTime = millis();
+  while (digitalRead(emergencyPin) == LOW) {
+    if ((millis() - startTime) > 15000) break;
+    delay(100);
+  }
 
-Serial.println("Rain Intensity: "+ ra);
-
-if(digitalRead(2)==HIGH){
-
-  digitalWrite(6,LOW);
-
-  digitalWrite(7,LOW);
-
-  delay(del);
-
-}
-
-if(digitalRead(9)==LOW){
-
-  tone(11, del); 
-
-  delay(100);    
-
-  noTone(11);     
-
- // delay(del00);     
-
-}
-
-re1();
-
-if(digitalRead(8)==LOW){
-
-  tone(11, del);
-
-  delay(100);       
-
-  noTone(11);    
-
- // delay(del00); 
-
-}
-
-re2();
-
-
-
-//noTone(11);
-
-
-
-
-
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(redPins[i], HIGH);
+  }
 }
